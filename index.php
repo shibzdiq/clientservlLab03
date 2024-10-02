b4d9fe33b687779cd93ad96389bb75f760c20908
shibzdiq <107814714+shibzdiq@users.noreply.github.com>
2024-10-02 15:14:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test PHP</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh; 
+                margin: 0;
+            }
+            h1 {
+                font-size: 48px; 
+                text-align: center;
+          }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo "Hello Stepanuk Mykhailo!"; ?></h1>
+    </body>
+</html>
